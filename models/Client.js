64f7463467ab7aa8699eea345d033abfc9da5d35
ChrisCueva3693 <xdcristian619@gmail.com>
2025-02-11 15:26:22 -0500

    change model

--- models/Client.js
+++ models/Client.js
@@ -6,6 +6,8 @@ const Cliente = sequelize.define('Cliente', {
     nombre: { type: DataTypes.STRING, allowNull: false },
     edad: { type: DataTypes.INTEGER, allowNull: false },
     peso: { type: DataTypes.FLOAT, allowNull: false },
+    telefono: { type: DataTypes.STRING, allowNull: false },
+    correo: { type: DataTypes.STRING, allowNull: false, unique: true },
     idsucursal: { type: DataTypes.INTEGER, allowNull: false }
 }, { timestamps: false });
 module.exports = Cliente;
